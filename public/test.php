7b4589a3daafef2c80de47c6612e0a70a0be2238
ramauf <ramoney@list.ru>
2016-03-15 19:18:20 +0500

    first

--- public/test.php
+++ public/test.php
@@ -1,3 +1,31 @@
 <?php
-var_dump(exec('/var/www/cstest.tk/.git/hooks/post-update'));
+error_reporting(E_ALL);
+//var_dump(exec('whereis ts'), file_exists('/var/www/cstest.tk/.git/hooks/post-update'));
+
+wsoEx('./qqq');
+
+
+function wsoEx($in) {
+	$out = '';
+	if (function_exists('exec')) {
+		@exec($in,$out);
+		$out = @join("\n",$out);
+	} elseif (function_exists('passthru')) {
+		ob_start();
+		@passthru($in);
+		$out = ob_get_clean();
+	} elseif (function_exists('system')) {
+		ob_start();
+		@system($in);
+		$out = ob_get_clean();
+	} elseif (function_exists('shell_exec')) {
+		$out = shell_exec($in);
+	} elseif (is_resource($f = @popen($in,"r"))) {
+		$out = "";
+		while(!@feof($f))
+			$out .= fread($f,1024);
+		pclose($f);
+	}
+	return $out;
+}
 ?>
